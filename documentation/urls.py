74f0540d6d5954ba703fb131fb1816f4a38aa89f
Mark Lysenko <58804992+SuperLalka@users.noreply.github.com>
2020-06-20 21:53:31 +0300

    Added 404' page for articles

--- documentation/urls.py
+++ documentation/urls.py
@@ -7,5 +7,6 @@ app_name = 'documentation'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^article/(?P<address>\w+)$', views.ArticleView.as_view(), name='article'),
+    url(r'^article_page_404$', views.article_404, name='article_404'),
     
 ]
--- documentation/views.py
+++ documentation/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import get_object_or_404, render
+from django.http import Http404
+from django.shortcuts import get_object_or_404, redirect, render
 from django.views import generic
 from .models import Article, Section
 
@@ -20,8 +21,24 @@ class ArticleView(generic.DetailView):
     slug_field = 'address'
     slug_url_kwarg = 'address'
 
+    def get(self, request, *args, **kwargs):
+        try:
+            return super().get(request, *args, **kwargs)
+        except Http404:
+            return redirect('/docs/article_page_404', request=None, permanent=True)
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['list_articles'] = Article.objects.order_by("-priority")
         context['list_section'] = Section.objects.all()
         return super().get_context_data(**context)
+
+
+def article_404(request):
+    list_articles = Article.objects.order_by("-priority")
+    list_section = Section.objects.all()
+    return render(
+        request,
+        'article_404.html',
+        context={'list_articles':list_articles,'list_section':list_section}
+    )
