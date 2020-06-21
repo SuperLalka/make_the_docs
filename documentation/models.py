788b325a12addf1397b08ddd63efe7d18424aea9
Mark Lysenko <58804992+SuperLalka@users.noreply.github.com>
2020-06-21 13:46:57 +0300

    Integrated tinymce from article.body

--- documentation/models.py
+++ documentation/models.py
@@ -1,12 +1,13 @@
 from datetime import datetime
 from django.db import models
 from django.urls import reverse
+from tinymce.models import HTMLField
 from .utils import transliterate
 
 
 class Article(models.Model):
     title = models.CharField(max_length=100, help_text="Enter a titles article")
-    body = models.TextField(help_text="Enter a text article")
+    body = HTMLField(help_text="Enter a text article")
     section = models.ForeignKey('Section', on_delete=models.SET_NULL, null=True)
     priority = models.IntegerField(default=0, null=True, blank=True)
     address = models.CharField(max_length=30, null=True, blank=True)
--- make_the_docs/settings.py
+++ make_the_docs/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'documentation.apps.DocumentationConfig',
+    'tinymce',
 ]
 
 MIDDLEWARE = [
