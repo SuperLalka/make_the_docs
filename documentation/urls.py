1b97d88f444b4de61f7d2bb41b6e3826c963d625
Mark Lysenko <58804992+SuperLalka@users.noreply.github.com>
2020-06-27 12:06:28 +0300

    Added global 404 page

--- documentation/urls.py
+++ documentation/urls.py
@@ -1,5 +1,6 @@
-from django.views.generic import RedirectView
 from django.conf.urls import url
+from django.views.generic import RedirectView
+
 from . import views
 
 
--- make_the_docs/urls.py
+++ make_the_docs/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import include, path
 
+from documentation import views
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('docs/', include('documentation.urls')),
